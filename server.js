cfa55876a78a77d3e9e88d17d95a0f56a90bcbbf
tylerduffy <duffy.tylerj@gmail.com>
2024-12-10 21:20:27 -0500

    updates to make server.js maybe work on heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-
-var app = express();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
@@ -8,7 +8,6 @@ app.use('/styles', express.static(__dirname + '/public/styles'));
 app.use('/scripts', express.static(__dirname + '/public/scripts'));
 app.use('/img', express.static(__dirname + '/public/img'));
 
-var server = app.listen(8081, function() {
-    var port = server.address().port;
-    console.log('Server started at http://localhost:%s', port);
+var server = app.listen(port, function() {
+    console.log('Server is running on port ${port}');
 });
